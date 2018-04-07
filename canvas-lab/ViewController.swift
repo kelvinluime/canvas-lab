32d96f0321de22680f6618e564b1a553d215ee04
Kelvin Lui <kelflo1120@gmail.com>
2018-04-06 18:21:03 -0700

    first

---
+++ canvas-lab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  canvas-lab
+//
+//  Created by Kelvin Lui on 4/6/18.
+//  Copyright © 2018 Kelvin Lui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
